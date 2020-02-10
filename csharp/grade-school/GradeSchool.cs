b8b353dce94a42bc290270f38baf2ff392a831a3
jackson ford <jfordnose64@gmail.com>
2020-02-10 12:25:19 -0500

    more daily C# work

---
+++ csharp/grade-school/GradeSchool.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace GradeSchool
+{
+  public class School
+  {
+    public School()
+    {
+      Roster = new Dictionary<int, List<string>>();
+    }
+
+    public Dictionary<int, List<string>> Roster { get; private set; }
+
+    public void Add(string student, int grade)
+    {
+      if (Roster.ContainsKey(grade))
+      {
+        Roster[grade].Add(student);
+        Roster[grade].Sort();
+      }
+      else
+      {
+        Roster.Add(grade, new List<string> { student });
+      }
+    }
+
+    public List<string> Grade(int grade)
+    {
+      var list = new List<string>();
+
+      if (Roster.ContainsKey(grade))
+      {
+        list = Roster[grade];
+      }
+
+      return list;
+    }
+  }
+}
